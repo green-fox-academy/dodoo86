08f8e1e335b9a972ead494d1ad07ae76ca852b60
Dodoo86 <l.davidtamas@gmail.comm>
2019-09-11 13:03:42 +0200

    task1

--- Week1/Day-3/Clion/Exercises/main.cpp
+++ Week1/Day-3/Clion/Exercises/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char const *argv [])
+{
+    std::cout << "Hello, Me" << std::endl;
+
+         std::cout << "Humpty Dumpty sat on a wall," << std::endl;
+         std::cout << "Humpty Dumpty had a great fall." << std::endl;
+         std::cout << "All the king's horses and all the king's men" << std::endl;
+         std::cout << "Couldn't put Humpty together again." << std::endl;
+
+
+             std::cout << "Hello Tamas" << std::endl;
+             std::cout << "Hello Laci"  << std::endl;
+             std::cout << "Hello Csaba" << std::endl;
+
+
+       
+
     return 0;
 }
