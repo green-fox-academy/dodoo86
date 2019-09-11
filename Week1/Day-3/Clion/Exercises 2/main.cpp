b7d8409b3778079d43875bc243c9e5b4eb37c8f5
Dodoo86 <l.davidtamas@gmail.comm>
2019-09-11 14:21:13 +0200

    task3

--- Week1/Day-3/Clion/Exercises 2/main.cpp
+++ Week1/Day-3/Clion/Exercises 2/main.cpp
@@ -2,9 +2,27 @@
 
 int main(int argc, char const *argv [])
 {
-    std::cout << "David Laszlo" << std::endl;
-    std::cout << "33" << std::endl;
-    std::cout << "1.92" << std::endl;
-    
+    //std::cout << "David Laszlo" << std::endl;
+    //std::cout << "33" << std::endl;
+    //std::cout << "1.92" << std::endl;
+
+    //std::cout << 13 + 22 << std::endl;
+    //std::cout << 22 - 13 << std::endl;
+    //std::cout << 22 * 13 << std::endl;
+    //int x = 22 / 13;
+    //std::cout << x <<std::endl;
+    //float z = (float) 22 / (float) 13;
+    //std::cout << z <<std::endl;
+    //std::cout << 22 % 13 << std::endl;
+
+
+    int daily = 6;
+    int Weeks = 17;
+            std::cout << (daily * Weeks) * 5  << std::endl;
+
+            float y = ((float)52 / (float)100)* 30 ;
+            std::cout << y <<std::endl;
+
+
     return 0;
 }
---
+++ Week1/Day-3/Clion/Exercises 3/cmake-build-debug/CMakeFiles/Exercises_3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Exercises_3.dir/main.cpp.o"
+  "Exercises_3.pdb"
+  "Exercises_3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Exercises_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
