1db05a316b1ee4ef2cfddb0ad699f7ef7ce2f3ae
Dodoo86 <l.davidtamas@gmail.comm>
2019-09-11 17:36:53 +0200

    end of day 3

--- Week1/Day-3/Clion/Exercises 4/main.cpp
+++ Week1/Day-3/Clion/Exercises 4/main.cpp
@@ -18,4 +18,26 @@ int main(int argc, char* args[]) {
     //double kilometers = miles * 1.6;
     //cout << kilometers << "Kilometers";
 
+    //int number;
+    //cout << "Give me a number ..." << endl;
+    //cin >> number;
+    //if (number % 2<=0){
+        //cout << "Your number is even";}
+    //else {
+    //cout << "Your number is not even" << endl;
+//}
+
+    //int number;
+    //cout << "Give me a number..." << endl;
+    //cin >> number;
+    //if (number <= 0) {
+        //cout << "Not enough";
+    //}else if (number == 1) {
+        //cout << "One" << endl;
+    //}else if (number == 2) {
+        //cout << "Two" << endl;
+    //}else
+        //cout << "Too much" << endl;
+
+
 }
