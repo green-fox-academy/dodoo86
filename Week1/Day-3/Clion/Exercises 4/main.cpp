a171b2eb1bd4fab9d0f7bcfd9df7eb11e731df1f
Dodoo86 <l.davidtamas@gmail.comm>
2019-09-11 17:12:15 +0200

    task4

---
+++ Week1/Day-3/Clion/Exercises 4/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char* args[]) {
+
+    //string name;
+    //cout << "What is Your name ?" << endl;
+    //cin >> name;
+    //cout << "Hello " + name << endl;
+
+
+
+    //double miles;
+    //cout << "How many miles ?" << endl;
+    //cin >> miles;
+    //double kilometers = miles * 1.6;
+    //cout << kilometers << "Kilometers";
+
+}
