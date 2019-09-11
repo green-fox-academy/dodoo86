1139ddf0d3a0c29053a143449856694f65da28c4
Dodoo86 <l.davidtamas@gmail.comm>
2019-09-11 13:08:34 +0200

    task2

---
+++ Week1/Day-3/Clion/Exercises 2/cmake-build-debug/CMakeFiles/Exercises_2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Exercises_2.dir/main.cpp.o"
+  "Exercises_2.pdb"
+  "Exercises_2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Exercises_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
