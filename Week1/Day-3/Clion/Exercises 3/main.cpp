9c2357343dabea65d91fc1f1a0a9b8c03dcadb72
Dodoo86 <l.davidtamas@gmail.comm>
2019-09-11 16:36:21 +0200

    task4

--- Week1/Day-3/Clion/Exercises 3/main.cpp
+++ Week1/Day-3/Clion/Exercises 3/main.cpp
@@ -1,6 +1,73 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char* args[])
+{
+
+    //int x = 11;
+    //std::cout << "My favourite number is " << x << std::endl;
+
+    //int a = 526;
+    //int b = 123;
+    //int c = 1;
+            //c = a;
+            //a = b;
+            //b = c;
+            //std::cout << a << std::endl;
+            //std::cout << b << std::endl;
+
+
+            //std::cout << "David Laszlo" << std::endl;
+            //int age = 33;
+            //std::cout << age << std::endl;
+            //double height = 1.92;
+            //std::cout << height << std::endl;
+            //bool marriage = false;
+            //std::cout << marriage << std::endl;
+
+
+            //int a = 3 + 10;
+            //std::cout << a << std::endl;
+
+            //int b = 100 - 7;
+            //std::cout << b << std::endl;
+
+            //int c = 44 * 2;
+            //std::cout << c << std::endl;
+
+            //int d  = 125/5;
+            //std::cout << d << std::endl;
+
+            //int e = 8 * 8 * 8;
+            //std::cout << e << std::endl;
+
+            //int f1 = 123;
+            //int f2 = 345;
+            //std::cout << bool (f1 > f2) << std::endl;
+
+            //int g1 = 350;
+            //int g2 = 200;
+            //std::cout << bool (g2 * 2 > g1) << std::endl;
+
+            //int h = 135798745;
+            //std::cout << bool (h % 11 < 0) << std::endl;
+
+            //int i1 = 10;
+            //int i2 = 3;
+            //std::cout << bool (i1 > i2 * i2 ) << std::endl;
+            //std::cout << bool (i1 > i2 * i2 * i2) << std::endl;
+
+            //int j = 1521;
+            //std::cout << bool (j / 3 ) << std::endl;
+            //std::cout << bool (j % 5 < 0) << std::endl;
+
+    //int currentHours = 14;
+    //int currentMinutes = 34;
+    //int currentSeconds = 42;
+        //std::cout << int (24 * 60 * 60) - (currentHours * 60 * 60 + currentMinutes * 60 + currentSeconds) << std::endl;
+
+
+
+
+
     return 0;
 }
