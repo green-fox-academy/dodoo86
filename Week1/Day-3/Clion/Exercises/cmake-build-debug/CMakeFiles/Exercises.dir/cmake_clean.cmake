8eae7c3cea9c6ca07d85b47c3464b28eb436bc2d
Dodoo86 <l.davidtamas@gmail.comm>
2019-09-11 11:59:06 +0200

    gyakorlas

---
+++ Week1/Day-3/Clion/Exercises/cmake-build-debug/CMakeFiles/Exercises.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Exercises.dir/main.cpp.o"
+  "Exercises.pdb"
+  "Exercises"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Exercises.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Week1/Day-3/Clion/Exercises/cmake-build-debug/CMakeFiles/Exercises.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Exercises.
+# This may be replaced when dependencies are built.
