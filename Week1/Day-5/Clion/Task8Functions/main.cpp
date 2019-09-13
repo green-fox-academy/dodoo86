89b451dc6a4570bb230a91bbe4406471b4ac4f67
Dodoo86 <l.davidtamas@gmail.comm>
2019-09-13 17:46:24 +0200

    task8e

--- Week1/Day-5/Clion/Task8Functions/main.cpp
+++ Week1/Day-5/Clion/Task8Functions/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <string>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+
+void doubling();
+
+int main(int argc, char* args[]) {
+
+    doubling();
+
+    return 0;
+}
+
+void doubling() {
+    std::cout << "" << std::endl;
+}
+
+
+
+
     return 0;
 }
