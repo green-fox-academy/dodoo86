803e57818d4466a3d603e6c04dbadd85e265c1fb
Dodoo86 <l.davidtamas@gmail.comm>
2019-09-15 22:23:49 +0200

    arraysdone

--- Week1/Day-5/Clion/Task 7/main.cpp
+++ Week1/Day-5/Clion/Task 7/main.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main(int argc, char* args[]) {
-    //THIRD
+    //THIRD---------------------------------------------------
     /*
     int q[4] = {4,5,6,7};
 
@@ -21,14 +21,14 @@ int main(int argc, char* args[]) {
     }
 
     */
-    //SUM ELEMENTS
+    //SUM ELEMENTS-------------------------------------------------
     /*
     int r[4] = {54,23,66,12};
 
         cout<< r[1]+r[2] <<endl;
 
      */
-    //CHANGE ELEMENT
+    //CHANGE ELEMENT-----------------------------------------------
     /*
     int s[6] = {1,2,3,8,5,6};
     int temp;
@@ -37,13 +37,13 @@ int main(int argc, char* args[]) {
     s[4]=temp;
     cout<<s[4]<<" is the forth element"<<endl;
     */
-    //INCREMENT ELEMENT
+    //INCREMENT ELEMENT---------------------------------------------
     /*
     int t[5] = {1,2,3,4,5};
         t[3] = t[3]+ 1;
         cout<<t[3]<<endl;
     */
-    //PRINT ELEMENTS
+    //PRINT ELEMENTS--------------------------------------------------
     /*
     int numbers[4]= {4,5,6,7};
 
@@ -51,7 +51,7 @@ int main(int argc, char* args[]) {
          cout<<numbers[i]<<endl;
      }
      */
-    //MATRIX
+    //MATRIX-----------------------------------------------------------
      /*
     int x,y;
     cout<< "Give me two number..."<<endl;
@@ -71,7 +71,7 @@ int main(int argc, char* args[]) {
         cout << endl;
     }
     */
-     //DOUBLE ITEMS
+     //DOUBLE ITEMS-----------------------------------------------------------------------------
     /*
      int numlist[5] = {3,4,5,6,7};
         cout<<numlist[0]*2<<","<<numlist[1]*2<<","<<numlist[2]*2<<","<<numlist[3]*2<<","<<numlist[4]*2<<endl;
@@ -82,7 +82,64 @@ int main(int argc, char* args[]) {
             cout<<numlist[i]*2<<endl;
         }
         */
+    //COLOURS--------------------------------------------------------------------------
+    /*
+    string colour[3][5] = {
+            {"lime", " forest green", " olive", " pale green", " spring green"},
+            {"orange red", " red", " tomato"},
+            {"orchid", " violet", " pink", " hot pink"}
+    };
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 5; ++j) {
+            cout << colour[i][j];
+        }   cout<<endl;
+    }
+     */
+    // APPEND--------------------------------------------------------------------------
+    /*
+    string animals[3] = {"koal","pand","zebr"};
+    for (const auto & animal : animals) {
+        cout << animal << "a" <<endl;
+    }
+    */
+    //SWAP ELEMENTS-------------------------------------------------------------------
+    /*
+    string abc[3] = {"first","second","third"};
+    cout << abc[2]<< " " << abc[1]<< " " << abc[0] <<endl;
 
+    string cba[3] = {"first","second","third"};
+        swap(cba[0],cba[2]);
+        for (int i = 0; i < 3; ++i){
+            cout << cba[i];
+            cout << endl;
+    }
+    */
+    //SUM ALL ELEMENTS-----------------------------------------------------------------
+    /*
+    int ai[5] = {3,4,5,6,7};
+        int sum;
+        for (int i = 0; i < 5; ++i){
+            sum = sum+ai[i];}
+    cout << sum <<endl;
+
+     // OTHER SOULTION
+
+    int ai[5] = {3,4,5,6,7};
+        cout<< ai[0] + ai[1] + ai[2] + ai[3] + ai[4] <<endl;
+
+    */
+    //REVERSE LIST-----------------------------------------------------------------------
+    /*
+    int aj [5]= {3,4,5,6,7};
+    int revaj[5];
+    for (int i = 0; i < 5 ; ++i){
+        revaj[i] = aj[4-i];
+    }
+    for (int j = 0; j < 5; ++j){
+        aj[j] = revaj[j];
+        cout << aj[j] <<endl;
+    }
+    */
 
     return 0;
 }
