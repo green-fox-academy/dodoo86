ca0956b23fe64cfdd74331e19c8da3657e4f7a8b
Dodoo86 <l.davidtamas@gmail.comm>
2019-09-16 13:29:48 +0200

    strings done

---
+++ Week2/Day-1/Data Structures/main.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+//int main(int argc, char* args[]){
+
+    //SIMPLEREPLACE--------------------------------------------------------
+    /*
+
+    string example("In a dishwasher far far away");
+    string str2="galaxy";
+    string str3="dishwasher";
+    size_t pos = example.find(str3);
+    size_t len = str3.length();
+    example.replace(pos,len,"galaxy");
+
+    cout << example << endl;
+
+     */
+    //URL FIXER------------------------------------------------------------
+    /*
+    string url("https//www.reddit.com/r/nevertellmethebots");
+    string srt2=":";
+    string str3="bots";
+    size_t pos=url.find(str3);
+    size_t len=str3.length();
+    url.replace(pos,len,"odds/");
+    //url.insert(5,":");
+    url.insert(5,srt2);
+
+    cout << url << endl;
+
+    */
+    //TAKES LONGER---------------------------------------------------------------------------------------
+    /*
+    string quote("Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law.");
+    string str2="always takes longer than ";
+    quote.insert(21,str2);
+    cout << quote << endl;
+    */
+
+    //TO DO PRINT ---------------------------------------------------------------------
+    /*
+    string todoText = " - Buy milk\n";
+    string str2="My todo : \n";
+    todoText.insert(0,str2);
+    string games= " - Download Games\n";
+    string str3="     - Diablo \n";
+
+    cout << todoText;
+    cout << games;
+    cout << str3 <<endl;
+
+    */
+    //REVERSE----------------------------------------------------------------------------
+
+        /*
+        void reverseStr(string& str)
+        {
+            int n = str.length();
+            for (int i = 0; i < n / 2; i++)
+                swap(str[i], str[n - i - 1]);
+        }
+        int main(int argc, char* args[])
+        {
+            string reverse = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+            reverseStr(reverse);
+            cout << reverse;
+
+        */
+
+            return 0;
+        }
