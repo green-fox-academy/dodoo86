36f5d792c1dee33c8ce8b47aa61528ef29391546
Dodoo86 <l.davidtamas@gmail.comm>
2019-09-16 22:11:50 +0200

    task4 finished

--- Week2/Day-1/DataStructures/main.cpp
+++ Week2/Day-1/DataStructures/main.cpp
@@ -64,10 +64,12 @@ int main(int argc, char* args[]) {
 
      */
     //LIST INTRODUCTION 2 -----------------------------------------------------
-
+    /*
     vector<string>listA = {"Apple", "Avocado", "Blueberries", "Durian", "Lychee"};
     vector<string>listB = {"Apple", "Avocado", "Blueberries", "Durian", "Lychee"};
     listA.insert(listA.begin()+3,"Kiwi");
+    listB.insert(listB.begin()+5,"Passion Fruit");
+    listB.insert(listB.begin()+6,"Pomelo");
     for (int y = 0; y < listA.size(); ++ y){
         cout << listA.at(y) << endl;
 
@@ -90,10 +92,56 @@ int main(int argc, char* args[]) {
     else if (listB.size()>listA.size()){
         cout << "List B has more fruits" <<endl;
     }
+    else cout <<"List A and list B has equal amounth of fruits" <<endl;
+        cout<<endl;
     for(int i=0; i<=listA.size(); ++i)
         if(listA[i]=="Avocado"){
             cout<<"The index number for "<<"Avocado"<<" is "<<i<<'\n';
         }
+    for(int u=0; u<=listB.size(); ++u)
+        if(listB[u]=="Durian") {
+            cout << "The index number for " << "Durian" << " is " << u << '\n';
+        }
+    cout<<endl;
+    cout<<listA[2]<<endl;
+    */
+
+    //MAP INTRODUCTION 2 -------------------------------------------------------------
+
+    /*
+    map<string,string>map1;
+
+    map1.insert({"978-1-60309-452-8", "A Letter to Jo"});
+    map1.insert({"978-1-60309-459-7", "Lupus"});
+    map1.insert({"978-1-60309-444-3"	, "Red Panda and Moon Bear"});
+    map1.insert({"978-1-60309-461-0", "The Lab"});
+    map1.erase("978-1-60309-444-3");
+    map1.insert({"978-1-60309-450-4", "They Called Us Enemy"});
+    map1.insert({"978-1-60309-453-5", "Why Did We Trust Him?"});
+
+    for (auto itr = map1.begin(); itr != map1.end(); ++itr) {
+        cout << itr->second <<
+                 '\t'<<"(ISBN: " <<itr->first <<")"<< '\n';
+
+    }
+    for (map<string,string>::iterator it = map1.begin();it != map1.end(); ++it){
+        if (it->second == "The Lab"){
+            map1.erase(it);
+        }
+    }
+    map<string,string>::iterator it3 = map1.find("478-0-61159-424-8");
+    if (it3 != map1.end()){
+        cout<< "ISBN Found"<<endl;
+    }else{
+        cout<< "ISBN Not found"<<endl;
+    }
+    for (pair<string,string>bookPair:map1){
+        if (bookPair.first == "978-1-60309-453-5"){
+            cout<<bookPair.second<<endl;
+        }
+    }
+
+    */
 
     return 0;
 }
