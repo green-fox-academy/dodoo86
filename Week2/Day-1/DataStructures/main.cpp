e5e823f3b3dae91c016d8262afc9dba637cb3a59
Dodoo86 <l.davidtamas@gmail.comm>
2019-09-16 17:56:38 +0200

    end of day

--- Week2/Day-1/DataStructures/main.cpp
+++ Week2/Day-1/DataStructures/main.cpp
@@ -64,5 +64,36 @@ int main(int argc, char* args[]) {
 
      */
     //LIST INTRODUCTION 2 -----------------------------------------------------
+
+    vector<string>listA = {"Apple", "Avocado", "Blueberries", "Durian", "Lychee"};
+    vector<string>listB = {"Apple", "Avocado", "Blueberries", "Durian", "Lychee"};
+    listA.insert(listA.begin()+3,"Kiwi");
+    for (int y = 0; y < listA.size(); ++ y){
+        cout << listA.at(y) << endl;
+
+    }
+        cout << endl;
+    vector<string>::iterator it = find(listA.begin(), listA.end(), "Durian");
+    if (it != listA.end()){
+        cout << "Element Found" << endl;}
+    else{
+        cout << "Element Not Found" << endl;}
+        cout << endl;
+        listB.erase(find(listB.begin(), listB.end(), "Durian"));
+        for (int x = 0;x < listB.size(); ++ x ){
+            cout << listB.at(x) << endl;
+        }
+        cout << endl;
+    if (listA.size()>listB.size()){
+        cout << "List A has more fruits" << endl;
+    }
+    else if (listB.size()>listA.size()){
+        cout << "List B has more fruits" <<endl;
+    }
+    for(int i=0; i<=listA.size(); ++i)
+        if(listA[i]=="Avocado"){
+            cout<<"The index number for "<<"Avocado"<<" is "<<i<<'\n';
+        }
+
     return 0;
 }
