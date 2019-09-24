ab08af499d71d13d4e1481fa5f0de328986afb71
Dodoo86 <l.davidtamas@gmail.comm>
2019-09-24 13:36:39 +0200

    file_manipulations

---
+++ Week3/Day-2/File-manipulations/cmake-build-debug/CMakeFiles/File_manipulations.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/File_manipulations.dir/main.cpp.o"
+  "File_manipulations.pdb"
+  "File_manipulations"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/File_manipulations.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Week3/Day-2/File-manipulations/cmake-build-debug/CMakeFiles/File_manipulations.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for File_manipulations.
+# This may be replaced when dependencies are built.
