4c56ae3e98f1510126871a0734168967644c81d4
Dodoo86 <l.davidtamas@gmail.comm>
2019-10-01 13:55:45 +0200

    Insstart

---
+++ Week4/Day-2/Instruments/cmake-build-debug/CMakeFiles/Instruments.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Instruments.dir/main.cpp.o"
+  "Instruments.pdb"
+  "Instruments"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Instruments.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
