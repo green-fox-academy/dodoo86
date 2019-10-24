19e34a20a22021301858e0e8942fa05aaa130b00
Dodoo86 <l.davidtamas@gmail.comm>
2019-10-24 15:07:14 +0200

    sandwich task completted

---
+++ Week6/Day - 4/Sandwich/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/*
+Create a sandwich struct
+It should store:
+ - name
+ - price (float)
+ - weight (float)
+Create a function which takes two parameters and returns the price of your order.
+The parameters should be:
+ - a sandwich struct
+ - and an integer that represents how many sandwiches you want to order
+*/
+typedef struct {
+    char name;
+    float price;
+    float weight;
+}Sandwich_a;
+
+float total(Sandwich_a sandwich,int price){
+    return price * sandwich.price;
+}
+
+int main() {
+    Sandwich_a Hamms = {"Burger",11.f,2.5f};
+
+    printf("%2f\n",total(Hamms,4));
+    return 0;
+}
